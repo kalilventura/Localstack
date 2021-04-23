a8aba61a2685b8df8ea6aa6fe8ee5670b692daaa
kalilventura <kalilventur@gmail.com>
2021-04-23 11:12:30 -0300

    Mudanca de nome do projeto

--- fileServer/src/main/java/br/com/github/kalilventura/eventos/FileServierApplication.java
+++ fileServer/src/main/java/br/com/github/kalilventura/eventos/FileServierApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventosApplication {
+public class FileServierApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Trying to initialize system ...");
-		SpringApplication.run(EventosApplication.class, args);
+		SpringApplication.run(FileServierApplication.class, args);
 		System.out.println("Initialized");
 	}
 
